92f402aef21893c93b63210989cb4200af1b69f7
peterstonw01 <peterogechukwu1@gmail.com>
2025-03-24 20:23:31 +0100

    added vercel url

--- app/apiCall/fetchEmployee.js
+++ app/apiCall/fetchEmployee.js
@@ -1,5 +1,5 @@
 export const fetchEmployee = async () => {
-  const response = await fetch("http://localhost:3000/api/employee", {
+  const response = await fetch("https://mongodb-crud-kri1.vercel.app/", {
     cache: "no-store",
   });
   const data = await response.json();
@@ -9,3 +9,15 @@ export const fetchEmployee = async () => {
   }
   return data;
 };
+
+// export const fetchEmployee = async () => {
+//   const response = await fetch("http://localhost:3000/api/employee", {
+//     cache: "no-store",
+//   });
+//   const data = await response.json();
+//   console.log(data);
+//   if (error) {
+//     throw new Error("could not fetch Employee");
+//   }
+//   return data;
+// };
